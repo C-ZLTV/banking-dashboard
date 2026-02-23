5eebba4476ff89c08ed044cbad0eda8eb2cc9e38
Zlatov Cristina <cristinazltv.dev@gmail.com>
2026-02-23 14:06:29 +0100

    chore: Fix components folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import "./App.css";
-import Table, { type Column } from "./components.tsx/Table";
+import Table, { type Column } from "./components/Table/Table";
 import cardList from "./test/API/cards-list.json";
 import columnsJson from "./test/API/card-table-columns.json";
-import Input from "./components.tsx/Input/Input";
+import Input from "./components/Input/Input";
 
 const columns: Column[] = columnsJson.columns as Column[];
 
