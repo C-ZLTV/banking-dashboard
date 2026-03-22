fdaed163331aebf413b586e0dc4188bcf6caed25
Zlatov Cristina <cristinazltv.dev@gmail.com>
2026-03-22 17:14:39 +0100

    feat: integrate Card in Dashboard page

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,14 +1,15 @@
 import Input from "../../components/Input/Input";
-import Table, { type Column } from "../../components/Table/Table";
+// import Table, { type Column } from "../../components/Table/Table";
 import cardList from "../../test/API/cards-list.json";
-import columnsJson from "../../test/API/card-table-columns.json";
+// import columnsJson from "../../test/API/card-table-columns.json";
 import "./Dashboard.scss";
+import Card from "../../components/Card/Card";
 
 // type DashboardProps = {
 // };
 
 function Dashboard() {
-  const columns: Column[] = columnsJson.columns as Column[];
+  // const columns: Column[] = columnsJson.columns as Column[];
 
   return (
     <>
@@ -17,7 +18,19 @@ function Dashboard() {
         <Input id="pan" label="Pan" />
         <Input id="State" label="State" />
       </div>
-      <Table dataSource={cardList.cards} columns={columns} />
+      <div className="cards-container">
+        {cardList.cards.map((card) => {
+          return (
+            <Card title={card.pan} actions={[<span>Go to Card actions</span>]}>
+              <div>{card.branch}</div>
+              <div>{card.status}</div>
+              <div>{card.activationDate}</div>
+              <div>{card.expiryDate}</div>
+            </Card>
+          );
+        })}
+      </div>
+      {/* <Table dataSource={cardList.cards} columns={columns} /> */}
     </>
   );
 }
